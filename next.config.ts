472ef23e6acb3c8049640b001acccc874436ac0e
Suraj Survase <suraj.contentlab@gmail.com>
2026-08-23 09:35:01 +0530

    Confirgure for Render static Site

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
   images: {
+    unoptimized: true,
     domains: ["images.unsplash.com", "via.placeholder.com"],
   },
 };
