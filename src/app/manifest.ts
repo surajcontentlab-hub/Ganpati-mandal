32d0ffef4ba70cb7e08c915b6f5913825cb85224
Suraj Survase <suraj.contentlab@gmail.com>
2026-08-26 16:22:25 +0530

    Update PWA manifest for PWABuilder compatibility

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -12,10 +12,25 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#f97316',
     icons: [
       {
-        src: '/icon',
-        sizes: 'any',
+        src: '/icon?size=192x192',
+        sizes: '192x192',
         type: 'image/png',
+        purpose: 'any'
       },
+      {
+        src: '/icon?size=512x512',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable'
+      }
     ],
+    screenshots: [
+      {
+        src: '/icon?size=512x512',
+        sizes: '512x512',
+        type: 'image/png',
+        form_factor: 'narrow'
+      }
+    ]
   }
 }
